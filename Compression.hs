812daeab45a934fe4a9e14dcd74fb16dac4e3711
Travis Poulsen <travis.poulsen@gmail.com>
2014-05-15 08:53:35 -0400

    Functioning compression

--- Compression.hs
+++ Compression.hs
@@ -3,7 +3,6 @@ import qualified Data.List as L (nub, intercalate)
 import qualified Data.Map as M (Map, lookup, fromList, toList)
 import Text.Regex.Posix
 import Data.Maybe (fromMaybe)
-import Data.List.Split (splitOn)
 
 type CompressionMap = M.Map String Int
 
@@ -14,19 +13,17 @@ buildCompressionDict = M.fromList . flip zip [0..] . uniques
 compressChunk :: CompressionMap -> [String] -> String
 compressChunk m t = unwords . map runCompression $ t
     where runCompression s
-            | s =~ "[A-Z][a-z]*"  = compressed s ++ "^"
-            | s =~ "[A-Z]+"       = compressed s ++ "!"
-            | s =~ "[a-zA-Z]+\\." = compressed s ++ " " ++ "."
-            | s =~ "[a-zA-Z]+\\;" = compressed s ++ " " ++ ";"
-            | s =~ "[a-zA-Z]+\\:" = compressed s ++ " " ++ ":"
-            | s =~ "[a-zA-Z]+\\," = compressed s ++ " " ++ ","
-            | s =~ "[a-zA-Z]+\\?" = compressed s ++ " " ++ "?"
-            | s =~ "[a-zA-Z]+\\!" = compressed s ++ " " ++ "!"
-            | s =~ "[a-zA-Z]+-[a-zA-Z]+" = hyphenHandler $ (\([x, y]) -> (x,y)) . splitOn "-" $ s
+            | s =~ "[A-Z][a-z]*"  = compressed ++ "^"
+            | s =~ "[A-Z]+"       = compressed ++ "!"
+            | s =~ "[a-zA-Z]+\\." = compressed ++ " " ++ "."
+            | s =~ "[a-zA-Z]+\\;" = compressed ++ " " ++ ";"
+            | s =~ "[a-zA-Z]+\\:" = compressed ++ " " ++ ":"
+            | s =~ "[a-zA-Z]+\\," = compressed ++ " " ++ ","
+            | s =~ "[a-zA-Z]+\\?" = compressed ++ " " ++ "?"
+            | s =~ "[a-zA-Z]+\\!" = compressed ++ " " ++ "!"
             | otherwise           = compressed 
-                where compressed  = show . fromMaybe (-1) . flip M.lookup m . justTheWord 
-                      justTheWord = map C.toLower . filter (`notElem` ".,:;!?") 
-                      hyphenHandler x = (compressed . fst $ x) ++ " - " ++ (compressed . snd $ x)
+                where compressed    = show . fromMaybe (-1) . flip M.lookup m $ justTheWord s 
+                      justTheWord s =  map C.toLower . filter (`notElem` ".,:;!?") $ s
 
 compressText :: CompressionMap -> String -> String
 compressText m t = L.intercalate " R " . map (compressChunk m . words) . lines $ t 
